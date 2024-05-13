db7e76eb480e4d134b76f621b6ca1c5370c85dce
NATHAN BIZINOTO DE OLIVEIRA <1091392323023@fatec-srv.local>
2024-05-13 20:32:00 -0300

    update

--- popup.js
+++ popup.js
@@ -1,8 +1,49 @@
 document.getElementById('generate').addEventListener('click', function() {
     var serialNumber = document.getElementById('serialNumber').value;
-    var oldCNPJ = document.getElementById('oldCNPJ').value;
-    var newCNPJ = document.getElementById('newCNPJ').value;
+    var oldCNPJ = formatCNPJ(document.getElementById('oldCNPJ').value);
+    var newCNPJ = formatCNPJ(document.getElementById('newCNPJ').value);
+
+    if (!validateCNPJ(oldCNPJ) || !validateCNPJ(newCNPJ)) {
+        showModal("Por favor, insira CNPJs válidos com exatamente 14 dígitos.", false);
+        return;
+    } else if (oldCNPJ === newCNPJ) {
+        showModal("O CNPJ antigo e o novo CNPJ não podem ser iguais.", false);
+        return;
+    }
 
     var resultText = `POS migrated: ${serialNumber}\nfrom old CNPJ: ${oldCNPJ}\nto new CNPJ: ${newCNPJ}`;
-    document.getElementById('result').innerText = resultText;
+    showModal(resultText, true);
 });
+
+function formatCNPJ(cnpj) {
+    return cnpj.replace(/[\D]/g, ''); // Remove todos os caracteres não numéricos
+}
+
+function validateCNPJ(cnpj) {
+    return cnpj.length === 14; // Verifica se tem exatamente 14 dígitos
+}
+
+function showModal(text, allowCopy) {
+    document.getElementById('modalText').innerText = text;
+    var button = document.getElementById('modalButton');
+    if (allowCopy) {
+        button.innerText = 'Copiar Texto';
+        button.onclick = copyText;
+    } else {
+        button.innerText = 'OK';
+        button.onclick = function() { document.getElementById('myModal').style.display = 'none'; };
+    }
+    document.getElementById('myModal').style.display = 'block';
+}
+
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('myModal').style.display = "none";
+});
+
+function copyText() {
+    navigator.clipboard.writeText(document.getElementById('modalText').innerText).then(function() {
+        showModal('Texto copiado com sucesso!', false);
+    }, function(err) {
+        showModal('Erro ao copiar texto: ' + err, false);
+    });
+}
